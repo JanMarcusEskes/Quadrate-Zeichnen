a1777d8f5920a60d48a5f159e5fdb6ccc70a083a
Jan-Marcus Eskes <jan-marcus@eskes.de>
2016-11-04 17:24:39 +0100

    Projektdateien hinzufügen.

---
+++ Quadrate_zeichnen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrate_zeichnen
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int breite, rand;
+      char lücke = 'X';
+
+      Console.Write("Aussengrenze: ");
+      int.TryParse(Console.ReadLine(), out breite);
+      Console.Write("Randbreite  : ");
+      int.TryParse(Console.ReadLine(), out rand);
+
+      for (int zeile = 0; zeile < breite; zeile++)
+      {
+        if (zeile < rand || zeile >= breite - rand)
+        {
+          Console.WriteLine(new string(lücke, breite));
+        }
+        else
+        {
+          Console.WriteLine(new string(lücke, rand) + new string(' ', breite - rand * 2) + new string(lücke, rand));
+        }
+      }
+      Console.Read();
+    }
+  }
+}
